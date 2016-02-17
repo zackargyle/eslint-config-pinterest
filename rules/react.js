05a03de86fdf2af2dc13d356514b491bf5f639ac
Chris Lloyd <chrislloyd@pinterest.com>
2016-02-16 22:54:48 -0800

    React rules.

---
+++ rules/react.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ecmaFeatures: {
+        'jsx': true
+    },
+    plugins: [
+        'react'
+    ],
+    rules: {
+        'no-unreferenced-globals': 0,
+        'react/jsx-uses-react': 2,
+        'react/jsx-uses-vars': 2
+    }
+};
