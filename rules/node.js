6fa20c50313a02cb38c1cb7c801868f57284a2e3
Chris Lloyd <chrislloyd@pinterest.com>
2016-02-16 22:54:04 -0800

    Node rules.

---
+++ rules/node.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        'node': true
+    },
+    ecmaFeatures: {
+        'modules': true
+    },
+    rules: {
+        'no-console': 0,
+        'no-process-exit': 0,
+        'no-octal': 0,
+        'no-octal-escape': 0
+    }
+};
