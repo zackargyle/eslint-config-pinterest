c76c34f567c3a18bbec059e6eca8c42a269989d2
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-04 11:37:30 -0800

    Add component sorting.
    It might be nice to introduce linter rules one at a time, eventually getting to the nice state. Starting with this single rule to see what the workflow would be like.

--- rules/react.js
+++ rules/react.js
@@ -8,6 +8,27 @@ module.exports = {
     rules: {
         'no-unreferenced-globals': 0,
         'react/jsx-uses-react': 2,
-        'react/jsx-uses-vars': 2
+        'react/jsx-uses-vars': 2,
+
+         // Enforce component methods order
+         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
+         'react/sort-comp': [2, {
+             'order': [
+                 'static-methods',
+                 'lifecycle',
+                 'handlers',
+                 'everything-else',
+                 'rendering',
+             ],
+             'groups': {
+                 'rendering': [
+                     '/^render.+$/',
+                     'render'
+                 ],
+                 'handlers': [
+                     '/^handle.+$/',
+                 ]
+             }
+         }],
     }
 };
