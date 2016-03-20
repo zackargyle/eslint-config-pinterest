d4749cd9ac2d3b2857c410819ad86e5d3e68596e
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-19 23:00:57 -0700

    Set closing bracket location to be after-props.

--- rules/react.js
+++ rules/react.js
@@ -17,7 +17,7 @@ module.exports = {
         'react/jsx-boolean-value': [2, 'never'],
         // Validate closing bracket location in JSX
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-closing-bracket-location.md
-        // 'react/jsx-closing-bracket-location': [2, 'line-aligned'],
+        'react/jsx-closing-bracket-location': [2, 'after-props'],
         // Enforce or disallow spaces inside of curly braces in JSX attributes
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-curly-spacing.md
         'react/jsx-curly-spacing': [2, 'never', { 'allowMultiline': true }],
