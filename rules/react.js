42384d9ee0e135b747381f08a32018ce882845cc
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-30 13:20:25 -0700

    Import rule config from eslint-plugin-pinterest into legacy.js.
    These were removed in: https://github.com/pinterest-web/eslint-plugin-pinterest/pull/7

--- rules/react.js
+++ rules/react.js
@@ -6,6 +6,33 @@ module.exports = {
         'react'
     ],
     rules: {
+        // Turn off legacy rules in legacy.js.
+        'pinterest/cssclassname-extends-from-parent': 0,
+        'pinterest/no-accessing-private-members': 0,
+        'pinterest/no-bind-within-jquery': 0,
+        'pinterest/no-callbacks-where-promises': 0,
+        'pinterest/no-commented-out-tests': 0,
+        'pinterest/no-data-options-dot-notation': 0,
+        'pinterest/no-direct-p-pubsub-subscribe-call-in-modules': 0,
+        'pinterest/no-duplicate-jquery-selectors': 0,
+        'pinterest/no-goog-provide': 0,
+        'pinterest/no-goog-require': 0,
+        'pinterest/no-invalid-binds': 0,
+        'pinterest/no-jquery-outside-of-render': 0,
+        'pinterest/no-jquery-var-without-$': 0,
+        'pinterest/no-listen-childListen-bind': 0,
+        'pinterest/no-module-member-variables': 0,
+        'pinterest/no-mutable-module-options': 0,
+        'pinterest/no-private-functions-without-annotation': 0,
+        'pinterest/no-reassigning-keywords': 0,
+        'pinterest/no-reassigning-of-pubsub-topic-in-module': 0,
+        'pinterest/no-reassigning-requires': 0,
+        'pinterest/no-referencing-$el': 0,
+        'pinterest/no-referencing-p-properties': 0,
+        'pinterest/no-this-without-bind': 0,
+        'pinterest/no-translation-outside-of-function': 0,
+        'pinterest/no-unassigned-requires': 0,
+        'pinterest/no-unhandled-promise-errors': 0,
         // Prevent missing displayName in a React component definition
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
         'react/display-name': [0, { 'ignoreTranspilerName': false }],
