3c7046762f45fe211e8170af0ef25ecf27734f61
Chris Lloyd <chrislloyd@pinterest.com>
2016-06-02 23:44:02 -0700

    Use airbnb config

--- rules/node.js
+++ rules/node.js
@@ -1,14 +1,5 @@
 module.exports = {
-    env: {
-        'node': true
-    },
-    ecmaFeatures: {
-        'modules': true
-    },
-    rules: {
-        'no-console': 0,
-        'no-process-exit': 0,
-        'no-octal': 0,
-        'no-octal-escape': 0
-    }
+    extends: [
+        'airbnb-base/rules/node'
+    ]
 };
